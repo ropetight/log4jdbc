102792a59d78b6bb5d5f46a216da538d5c4503c7
Petar Cacic <pcacic@tis.hr>
2016-05-18 15:05:53 +0200

    Nebeans project files

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -4,9 +4,9 @@ setlocal
 REM (JDBC 3 version removed as of 2015-09-30)
 REM invoke the 1.6 jvm for the JDBC 4 version
 
-set ANT_HOME=c:\apache-ant-1.8.1
-set JAVA_HOME=C:\jdk\1.6.0_21
-set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
+set ANT_HOME=c:\work\apache-ant-1.9.6
+set JAVA_HOME=c:\Program Files\Java\jdk1.7.0_80
+set PATH="%JAVA_HOME%"\bin;%ANT_HOME%\bin
 
 call java -version
-call ant -Djdbc.level=4 -Djvm.ver=1.6 all
+call ant -Djdbc.level=4 -Djvm.ver=1.7 all
